9a61112b2105cf1a1cb9b972e791ec0661747432
Renzo <renzol2@illinois.edu>
2021-08-10 16:29:45 -0500

    Ensure jest ignores Cypress and change integration test structure

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,4 +1,4 @@
-context('App', () => {
+context('The home page', () => {
   it('should load our app and show context', () => {
     cy.visit('http://localhost:3000');
     cy.contains('Lineage');
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // https://dev.to/ashconnolly/how-to-quickly-add-jest-to-your-next-js-app-1h32
+  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+  modulePathIgnorePatterns: ['./cypress'],
   roots: ['<rootDir>/src'],
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
