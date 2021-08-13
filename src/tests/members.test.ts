d1782bb9b0691fe439838db159e9f6e7bdaf50b6
Renzo <renzol2@illinois.edu>
2021-08-13 03:23:49 -0500

    Finish boilerplate for test suite

--- src/tests/members.test.ts
+++ src/tests/members.test.ts
@@ -1,18 +1,18 @@
 import { addMember, MEMBERS_COL } from '../firebase/member';
 import { db, fb } from '../firebase/config';
 
-const TEST_MEMBER = 'Member1';
+const MEMBER_1 = 'Member1';
+const MEMBER_2 = 'Member2';
 const INITIAL_COUNT = 0;
 const CLASS_OF = '2023';
 const collection = db.collection(MEMBERS_COL);
 
 describe('addMember()', () => {
-
   it('should add the passed-in member to the database', async () => {
     const memberCount = (await collection.get()).size;
 
     const param = {
-      name: TEST_MEMBER,
+      name: MEMBER_1,
       classOf: CLASS_OF,
       adings: INITIAL_COUNT,
       aks: INITIAL_COUNT,
@@ -30,18 +30,33 @@ describe('addMember()', () => {
     const addedMemberSnapshot = await collection.doc(resultId).get();
     expect(addedMemberSnapshot.exists).toBe(true);
     expect(addedMemberSnapshot.id).toEqual(resultId);
-    
+
     const addedMemberData: any = addedMemberSnapshot.data();
-    expect(addedMemberData.name).toEqual(TEST_MEMBER);
+    expect(addedMemberData.name).toEqual(MEMBER_1);
     expect(addedMemberData.classOf).toEqual(CLASS_OF);
     expect(addedMemberData.aks).toEqual(INITIAL_COUNT);
     expect(addedMemberData.adings).toEqual(INITIAL_COUNT);
   });
 
+  it('should prevent members of duplicate names to be added', async () => {
+    // const memberCount = (await collection.get()).size;
+    // const param = {
+    //   name: MEMBER_2,
+    //   classOf: CLASS_OF,
+    //   adings: INITIAL_COUNT,
+    //   aks: INITIAL_COUNT,
+    // };
+    expect(false).toBe(true);
+  });
+
   afterAll(async () => {
     // Delete all test members
-    const result = await collection.where('name', '==', TEST_MEMBER).get();
-    await Promise.all(result.docs.map((doc) => collection.doc(doc.id).delete()));
+    const deleteMemberQuery = await collection
+      .where('name', 'in', [MEMBER_1, MEMBER_2])
+      .get();
+    await Promise.all(
+      deleteMemberQuery.docs.map((doc) => collection.doc(doc.id).delete())
+    );
     return await Promise.all(fb.apps.map((app) => app.delete()));
   });
 });
