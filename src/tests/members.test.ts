d977488840db283f0f9a11a86c0bb8fd202524c7
Renzo <renzol2@illinois.edu>
2021-08-13 03:16:46 -0500

    Expand first `addMember()` success test

--- src/tests/members.test.ts
+++ src/tests/members.test.ts
@@ -2,27 +2,40 @@ import { addMember, MEMBERS_COL } from '../firebase/member';
 import { db, fb } from '../firebase/config';
 
 const TEST_MEMBER = 'Member1';
+const INITIAL_COUNT = 0;
+const CLASS_OF = '2023';
+const collection = db.collection(MEMBERS_COL);
 
 describe('addMember()', () => {
-  const collection = db.collection(MEMBERS_COL);
 
   it('should add the passed-in member to the database', async () => {
     const memberCount = (await collection.get()).size;
 
     const param = {
       name: TEST_MEMBER,
-      classOf: '2023',
-      adings: 0,
-      aks: 0,
+      classOf: CLASS_OF,
+      adings: INITIAL_COUNT,
+      aks: INITIAL_COUNT,
     };
 
     const resultDoc = await addMember(param);
-    expect(resultDoc).not.toBe(undefined);
+    expect(resultDoc).not.toBeUndefined();
+    const resultId = resultDoc?.id;
+    expect(resultId).not.toBeUndefined();
 
     const newMemberCount = (await collection.get()).size;
     expect(newMemberCount).toEqual(memberCount + 1);
 
-    // TODO: ensure that the member is actually in the database
+    // Ensure that the member is correctly added into the database
+    const addedMemberSnapshot = await collection.doc(resultId).get();
+    expect(addedMemberSnapshot.exists).toBe(true);
+    expect(addedMemberSnapshot.id).toEqual(resultId);
+    
+    const addedMemberData: any = addedMemberSnapshot.data();
+    expect(addedMemberData.name).toEqual(TEST_MEMBER);
+    expect(addedMemberData.classOf).toEqual(CLASS_OF);
+    expect(addedMemberData.aks).toEqual(INITIAL_COUNT);
+    expect(addedMemberData.adings).toEqual(INITIAL_COUNT);
   });
 
   afterAll(async () => {
