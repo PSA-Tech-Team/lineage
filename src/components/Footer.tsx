be3cd23f3d30235c3e84a5a2b9ba1a1514d3c120
Renzo <renzol2@illinois.edu>
2021-08-27 15:23:45 -0500

    Finish styling for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
-import { Box, Center, Grid, Heading, Text } from '@chakra-ui/react';
+import { Box, Center, Container, Grid, Heading, Text } from '@chakra-ui/react';
+import FooterLink from './FooterLink';
 
 const Footer = () => (
   <footer>
@@ -8,7 +9,7 @@ const Footer = () => (
       py={{ base: '2rem', md: '8rem' }}
     >
       <Center>
-        <Box color="whitesmoke">
+        <Box color="whitesmoke" w={{ base: '85%', md: '60%' }}>
           <Heading as="h3" fontWeight="black">
             Lineage
           </Heading>
@@ -19,6 +20,21 @@ const Footer = () => (
           <Text fontWeight="light">
             © {new Date().getFullYear()} Philippine Student Association
           </Text>
+        </Box>
+      </Center>
+      <Center color="whitesmoke" mt={{ base: '2rem', md: '0px' }}>
+        <Box w={{ base: '85%', md: '60%' }}>
+          <Heading as="h3" fontSize="3xl">
+            Links
+          </Heading>
+          <Grid templateColumns={{ base: '100%', md: '1fr 1fr' }}>
+            <FooterLink href="/lineages">Lineages</FooterLink>
+            <FooterLink href="/edit">Dashboard</FooterLink>
+            <FooterLink href="https://github.com/renzol2/lineage">
+              GitHub
+            </FooterLink>
+            <FooterLink href="http://psauiuc.org/">PSA UIUC</FooterLink>
+          </Grid>
         </Box>
       </Center>
     </Grid>
