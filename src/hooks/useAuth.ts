eb814fd6755503f6b2ba3eadfa66f8d5f7926f6c
Renzo <renzol2@illinois.edu>
2021-09-09 00:52:22 -0500

    Return user info in auth hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,8 +1,8 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../firebase/config";
-import { isBoardMember, isEditor } from "../fixtures/Editors";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase/config';
+import { isBoardMember, isEditor } from '../fixtures/Editors';
 
 /**
  * Hook for routing the user to login depending on auth state.
@@ -28,17 +28,21 @@ const useAuth = () => {
     // Checks if PSA board member or is allowed to edit
     const isValidEmail = isEditor(user.email) || isBoardMember(user.email);
     const isValidUser = user !== null && isValidEmail;
-    console.log({isValidUser});
 
     if (!isValidUser) {
       setValidUser(false);
       router.push('/login');
     } else {
-      setValidUser(true)
+      setValidUser(true);
     }
   }, [user, userLoading]);
 
-  return { user, isValidUser, userLoading, loadSplash: userLoading || !isValidUser };
+  return {
+    user,
+    isValidUser,
+    userLoading,
+    loadSplash: userLoading || !isValidUser,
+  };
 };
 
 export default useAuth;
