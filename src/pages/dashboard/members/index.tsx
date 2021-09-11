7687b2585f064e69e380218578ebef1327f3bbf4
Renzo <renzol2@illinois.edu>
2021-09-11 01:45:40 -0500

    Create layout for members page

---
+++ src/pages/dashboard/members/index.tsx
@@ -0,0 +1,27 @@
+import { Grid, Heading } from '@chakra-ui/layout';
+import DashboardCard from '../../../components/DashboardCard';
+import DashboardWrapper from '../../../components/DashboardWrapper';
+import { YEARS } from '../../../fixtures/Semesters';
+
+const NAVIGATION = YEARS.map((year) => ({
+  title: `Class of ${year}`,
+  description: `View members from class of ${year}`,
+  href: '/dashboard/members',
+}))
+
+const DashboardPage = () => {
+  return (
+    <DashboardWrapper>
+      <Heading mb="3rem" fontWeight="light">
+        View members
+      </Heading>
+      <Grid templateColumns={{ base: '100%', md: 'repeat(3, 1fr)' }} gap={6}>
+        {NAVIGATION.map(({ title, description, href }) => (
+          <DashboardCard title={title} description={description} href={href} />
+        ))}
+      </Grid>
+    </DashboardWrapper>
+  );
+};
+
+export default DashboardPage;
